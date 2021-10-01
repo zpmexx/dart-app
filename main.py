27ae722a79bdbc0fb43f5c68191ce32f5f4713b3
Ziem <ziemowitpalka@gamil.com>
2021-10-01 22:32:33 +0200

    need to bind buttons to proper id in chooseplayers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.core import text
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
@@ -9,53 +10,112 @@ from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from random import randint
 
 class SoloWindow(Screen):
-    pass
+    def printhi(self):
+        print("witam")
 
 class GameWindow(Screen):
-    def getplayers(self):
-        print(self.manager.get_screen('chooseplayers').playerGrid.children)
-        for i in range (0,len(self.manager.get_screen('chooseplayers').playerGrid.children),2):
-            print (self.manager.get_screen('chooseplayers').playerGrid.children[i].text)
-        self.manager.get_screen('chooseplayers').playerGrid.clear_widgets()
-        self.manager.get_screen('chooseplayers').playersInput.text = ''
-        self.manager.get_screen('chooseplayers').playersButton.disabled = False
+    def create(self):
+        self.usersGrid.clear_widgets()
+        count = len(self.manager.get_screen('chooseplayers').playerGrid.children)
+        for i in range (0,count,2):
+            x = Label()
+            x.text = self.manager.get_screen('chooseplayers').playerGrid.children[i].text
+            self.usersGrid.add_widget(x)
+            x.id = i
+            x = Label()
+            x.text = str(501)
+            self.usersGrid.add_widget(x)
+            x.id = i+1
+            
+    # testlab = ObjectProperty(None)
+    # def __init__(self, **kwargs):
+    #     super(GameWindow, self).__init__(**kwargs)
+    #     self.lbl = self.ids['testlab']
+    # def getplayers(self):
+    #     print(self.manager.get_screen('chooseplayers').playerGrid.children)
+    #     for i in range (0,len(self.manager.get_screen('chooseplayers').playerGrid.children),2):
+    #         print (self.manager.get_screen('chooseplayers').playerGrid.children[i].text)
+    #     print(f'dlugosc to: ',len(self.manager.get_screen('chooseplayers').playerGrid.children) )
+    #     self.manager.get_screen('chooseplayers').playerGrid.clear_widgets()
+    #     self.manager.get_screen('chooseplayers').playersInput.text = ''
+    #     self.manager.get_screen('chooseplayers').playersButton.disabled = False
+    #     # self.testlab.clear_screen()
+    #     # self.ids.testlab.text = 'Elo'
+    #     x =( Label(
+    #     name = 'testlab',
+    #     size_hint = (0.2, 0.2),
+    #     pos_hint ={'x':0.4 , 'y':0.8 }))
+    #     self.add_widget(x)
+    #     x.id = 'testlab'
+    #     self.testlab.text = 'witam'
 
+    # def testm(self):
+    #     self.testlab.text = 'chuj'
+    
 class ChoosePlayers(Screen):
-    global players
-    playersInput = ObjectProperty(None)
-    playersButton = ObjectProperty(None)
-    playersLabel = ObjectProperty(None)
-    def checkPlayers(self):
-        # if len(self.playersInput.text)== 0 or len(self.playersInput.text) > 1:
-        #     return False,'err'
-        # for i in self.playersInput.text:
-        #     if i.isdigit() == True:
-        #         if int(i) > 1 and int(i) < 9:
-        #             return True,i
-        #     else:
-        #         return False,i
-        # return False,i
-        number = self.ids.playersInput.text
-        for i in range (0,int(number)):
+    # global players
+    # playersInput = ObjectProperty(None)
+    # playersButton = ObjectProperty(None)
+    # playersLabel = ObjectProperty(None)
+    # def checkPlayers(self):
+    #     # if len(self.playersInput.text)== 0 or len(self.playersInput.text) > 1:
+    #     #     return False,'err'
+    #     # for i in self.playersInput.text:
+    #     #     if i.isdigit() == True:
+    #     #         if int(i) > 1 and int(i) < 9:
+    #     #             return True,i
+    #     #     else:
+    #     #         return False,i
+    #     # return False,i
+    #     number = self.ids.playersInput.text
+    #     for i in range (0,int(number)):
+    #         self.playerGrid.add_widget(Label(text='Gracz '+str(i+1)))
+    #         self.playerGrid.add_widget(TextInput(multiline = False,name = str(i+1)))
+    #     self.playersButton.disabled = True
+
+    def buttonclicked(self,i):
+        print(i)
+        self.playerGrid.clear_widgets()
+        for i in range (0,i):
             self.playerGrid.add_widget(Label(text='Gracz '+str(i+1)))
             self.playerGrid.add_widget(TextInput(multiline = False,name = str(i+1)))
-        self.playersButton.disabled = True
+        return i
+    
+    def create(self):
+        self.chooseGrid.clear_widgets()
+        for i in range(2,9):
+            x = Button()
+            x.text = str(i)
+            x.id = i
+            # x.bind(on_release = lambda x: self.buttonclicked(i))
+            # x.bind(on_release = lambda x: self.buttonclicked(randint(2,9)))
+            self.chooseGrid.add_widget(x)
+        num = 9
+        for i in self.chooseGrid.children:
+            print(i)
+            i.bind(on_release = lambda x:self.buttonclicked(num))
+            # print(self.buttonclicked(num))
+            # print(i.id)
+            num -= 1
+        # for i in self.chooseGrid.children:
+        #     print (i.id)
 
-    def clear(self):
-        self.playerGrid.clear_widgets()
-        self.playersInput.text = ''
-        self.playersButton.disabled = False
+    # def clear(self):
+    #     self.playerGrid.clear_widgets()
+    #     self.playersInput.text = ''
+    #     self.playersButton.disabled = False
 
-    def saveNames(self):
-        players = [] 
-        for i in range (0,len(self.playerGrid.children),2):
-            players.append(self.playerGrid.children[i].text)
-            print(self.playerGrid.children[i].text)
-        print(players)
-        return players 
-        # print(self.manager.get_screen('main').solobutton.text)
+    # def saveNames(self):
+    #     players = [] 
+    #     for i in range (0,len(self.playerGrid.children),2):
+    #         players.append(self.playerGrid.children[i].text)
+    #         print(self.playerGrid.children[i].text)
+    #     print(players)
+    #     return players 
+    #     # print(self.manager.get_screen('main').solobutton.text)
 
         
 
